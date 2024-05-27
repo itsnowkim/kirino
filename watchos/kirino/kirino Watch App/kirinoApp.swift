1441b23b9e527273dba2404376b7816eee5eb13c
Hyunjae Kim <peterhyunjae@naver.com>
2024-05-27 13:30:39 +0900

    [Create] watch os 프로젝트 init

---
+++ watchos/kirino/kirino Watch App/kirinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kirinoApp.swift
+//  kirino Watch App
+//
+//  Created by 김현재 on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct kirino_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
