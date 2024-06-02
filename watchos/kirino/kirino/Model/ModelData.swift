b258dcd383a579ae749966a86055bb195d453765
Hyunjae Kim <peterhyunjae@naver.com>
2024-06-02 18:53:43 +0900

    [feat] : modeldata updated

---
+++ watchos/kirino/kirino/Model/ModelData.swift
@@ -0,0 +1,35 @@
+//
+//  ModelData.swift
+//  kirino
+//
+//  Created by 김현재 on 5/28/24.
+//
+
+import Foundation
+
+@Observable
+class ModelData {
+    var messages: [Message] = load("messageData.json")
+}
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
